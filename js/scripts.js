087261c1029ef91262f669e3be063e24f40b70eb
Jacqueline Remmel and sofia giordano <sofia@>
2019-08-28 16:28:07 -0700

    comment out for later review

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // FRONT END
+// global var, creates an instance of the game object called myGame
 var myGame = new Game();
 
+// this attaches click listeners to the cards in our html
 function attachClickListeners() {
   $(".col-xs-4").on("click", ".card", function() {
     var clicked = this.id;
@@ -8,22 +10,26 @@ function attachClickListeners() {
   });
 }
 
+// this is how we stop the same space from getting clicked twice.
 function determineClickedSpace(id) {
+  // id refers to the parameter of this function, this function is called above, but defined here
   if (!$("." + id).text()) {
     myGame.logClicks(id);
   }
 }
-
+// this pushes an X or O (depending on who clicked) onto the html
 function displayMark(id, symbol) {
   $("." + id).text(symbol);
 }
 
 $(document).ready(function() {
+  // click listener is what tells us which space on the board has been clicked
   attachClickListeners();
   $(".gameForm").submit(function(event) {
     event.preventDefault();
     var playerXname = $("#playerXname").val();
     var playerOname = $("#playerOname").val();
+    // this is where we take the input values (player's names) and assign them to the name property of each player object
     myGame.playerX.name = playerXname;
     myGame.playerO.name = playerOname;
   });
@@ -31,17 +37,22 @@ $(document).ready(function() {
 
 
 // BACK END
+// game constructor
 function Game() {
   this.board = new Board();
   this.playerX = new Player();
   this.playerO = new Player();
+  // this is an empty number var we start at zero so that we can add "clicks" to it
   this.clickCounter = 0;
 }
 
+// this method logs the clicks and increase 1 to the clickCounter var.
 Game.prototype.logClicks = function(htmlID) {
   console.log(this.clickCounter++);
+  // the % will return a 0 or a 1 depending on whether the clickCounter is even or odd
   if (this.clickCounter % 2 === 0) {
     this.playerX.clicks.push(htmlID);
+    // loops through playerX's click array to mark clicked spaces with an X
     for (var i = 0; i < this.playerX.clicks.length; i++) {
       displayMark(this.playerX.clicks[i], "X");
     }
@@ -64,6 +75,7 @@ Player.prototype.assignSymbol = function() {
 
 }
 
+// ths is our grid- we made coords. based off the cols and rows
 function Board() {
   this.spaceC1R1 = "c1r1";
   this.spaceC2R1 = "c2r1";
