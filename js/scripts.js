2d2084b26b67f751590d4f9afac33b2c583ed8a0
Jacqueline Remmel and sofia giordano <sofia@>
2019-08-28 10:26:16 -0700

    Add backend JS constructors and basic grid in html.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,38 @@ $(document).ready(function(){
 
 
 // BACK END
-function Game (playerX, playerO, board){
-  this.playerX =
-  this.playerO =
-  this.board =
+function Game (playerX, playerO) {
+  this.playerX,
+  this.playerO
 
+}
+
+function Board () {
+
+}
+
+Board.prototype.makeGrid = function() {
+  //method to create 3x3 grid of spaces
+}
+
+Board.prototype.findWinner = function() {
+  //look for three in a row
+}
+
+function Space (col, row) {
+  this.col = col;
+  this.row = row;
+}
+
+Space.prototype.mark(player) {
+  //method for being marked by either playerX or playerO
+}
+
+function Player (name, symbol) {
+  this.name = name;
+  this.symbol = symbol;
+}
+
+Player.prototype.symbol () {
+  return this.symbol;
 }
