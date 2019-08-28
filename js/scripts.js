245bea15bb7bde415f13588e623610a98b376802
Jacqueline Remmel and sofia giordano <jacqueline@>
2019-08-28 15:20:19 -0700

    add x/o symbols to clicked spots.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 // FRONT END
 var myGame = new Game();
 console.log(myGame);
+
 function attachClickListeners() {
   $(".col-xs-4").on("click", ".card", function() {
     var clicked = this.id;
-    myGame.logClicks("TEST");
+    myGame.logClicks(clicked);
     console.log(clicked);
     console.log(myGame.increaseClickCount());
   });
 }
 
-$(document).ready(function(){
+function displayMark(id, symbol) {
+  $("." + id).text(symbol);
+}
+
+$(document).ready(function() {
   attachClickListeners();
-  $(".gameForm").submit(function(event){
+  $(".gameForm").submit(function(event) {
     event.preventDefault();
     var playerXname = $("#playerXname").val();
     var playerOname = $("#playerOname").val();
@@ -23,30 +28,36 @@ $(document).ready(function(){
 
 
 // BACK END
-function Game () {
+function Game() {
   this.board = new Board();
   this.playerX = new Player();
   this.playerO = new Player();
   this.clickCounter = 0;
 }
 
-Game.prototype.increaseClickCount = function () {
+Game.prototype.increaseClickCount = function() {
   return this.clickCounter++
 }
 
-Game.prototype.logClicks = function (htmlID) {
+Game.prototype.logClicks = function(htmlID) {
   if (this.clickCounter % 2 === 0) {
     this.playerX.clicks.push(htmlID);
-    console.log(this.playerX.clicks);
-    return this.playerX.clicks;
+    for (var i = 0; i < this.playerX.clicks.length; i++) {
+      displayMark(this.playerX.clicks[i], "X");
+    }
+
+
   } else if (this.clickCounter % 2 === 1) {
     this.playerO.clicks.push(htmlID);
-    console.log(this.playerO.clicks);
-    return this.playerO.clicks;
+    for (var i = 0; i < this.playerO.clicks.length; i++) {
+      displayMark(this.playerO.clicks[i], "O");
+    }
+
+
   }
 }
 
-function Player () {
+function Player() {
   this.name;
   this.symbol;
   this.clicks = [];
@@ -57,7 +68,7 @@ Player.prototype.assignSymbol = function() {
 
 }
 
-function Board () {
+function Board() {
   this.spaceC1R1 = "c1r1";
   this.spaceC2R1 = "c2r1";
   this.spaceC3R1 = "c3r1";
